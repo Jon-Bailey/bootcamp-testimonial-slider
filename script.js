321af366cfd8a16f74019ebc1b9b0b2ea973fdfd
Bailey <jbailey@budcentral.com>
2021-06-10 14:37:02 -0500

    buttons and script file added

---
+++ script.js
@@ -0,0 +1,8 @@
+const tanyaSection = document.getElementById("tanya-section");
+const johnSection = document.getElementById("john-section");
+const previous = document.getElementById("icon-prev");
+const next = document.getElementById("icon-next");
+
+// when buttons are pressed, toggle sections from display block to display none
+switchProfile = () => {
+}
