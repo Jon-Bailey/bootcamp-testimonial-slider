d15c1bcba43adbd39b229c2db8cc5ed71fd97460
Bailey <jbailey@budcentral.com>
2021-06-11 10:51:06 -0500

    footer styling

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 const tanyaSection = document.getElementById("tanya-section");
 const johnSection = document.getElementById("john-section");
-const previous = document.getElementById("icon-prev");
-const next = document.getElementById("icon-next");
 
 // when buttons are pressed, toggle sections from display block to display none
 switchProfile = () => {
