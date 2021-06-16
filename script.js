6517f2a41ad3c0a0b8263aba5ab3f03c430d72dd
Bailey <jbailey@budcentral.com>
2021-06-16 15:48:07 -0500

    starting desktop width

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 const tanyaSection = document.getElementById("tanya-section");
 const johnSection = document.getElementById("john-section");
 
+/*const testimonials = [
+    {
+        id:0,
+        name:"John Tarkpor",
+        text:' \" If you want to lay the best foundation possible I\'d recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer. \" ',
+        image:"images/image-john.jpg",
+        title:"Junior Front-end Developer"
+    },
+    {
+        id:1,
+        name:"Tanya Sinclair",
+        text:' \" I\'ve been interested in coding for a while but never taken the jump, until now. I couldn\'t recommend this course enough. I\'m now in the job of my dreams and so excited about the future. \" ',
+        image:"images/image-tanya.jpg",
+        title:"UX Engineer"
+    }];
+
+    document.querySelector("#paragraph").innerHTML = testimonials[0].text;
+*/
+
 // when buttons are pressed, toggle sections from display block to display none
 switchProfile = () => {
     if (johnSection.style.display === "none") {
@@ -11,3 +30,13 @@ switchProfile = () => {
         tanyaSection.style.display = "block";
     }
 }
+
+/*
+toggleVisible = () => {
+    if (this.parentElement.style.visibility === "visible") {
+        this.parentElement.classList.add(".m-fadeIn");
+    } else {
+        this.parentElement.classList.add(".m-fadeOut");
+    }
+}
+*/
